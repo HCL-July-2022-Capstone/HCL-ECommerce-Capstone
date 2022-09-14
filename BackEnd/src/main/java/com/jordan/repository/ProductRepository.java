ad1d566382de99ee49f14d74ea81c1e0cb16b11a
Sumaiya S <sum.shefa@gmail.com>
2022-09-14 00:49:22 -0500

    Update ProductRepository.java

--- BackEnd/src/main/java/com/jordan/repository/ProductRepository.java
+++ BackEnd/src/main/java/com/jordan/repository/ProductRepository.java
@@ -9,4 +9,5 @@ public interface ProductRepository extends JpaRepository<Product, Integer>{
 //	@Modifying
 //	@Query("update User u set u.firstname = ?1, u.lastname = ?2 where u.id = ?3")
 //	void setUserInfoById(String firstname, String lastname, Integer userId);
+	List<Product> getByCategoryName(String categoryName);
 }
