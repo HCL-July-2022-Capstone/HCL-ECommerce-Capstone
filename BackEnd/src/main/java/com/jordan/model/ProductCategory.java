8b4f50ff22cdc36a030593ea9256744343ac3280
Sumaiya S <sum.shefa@gmail.com>
2022-09-14 15:01:36 -0500

    Update ProductCategory.java

--- BackEnd/src/main/java/com/jordan/model/ProductCategory.java
+++ BackEnd/src/main/java/com/jordan/model/ProductCategory.java
@@ -1,31 +1,31 @@
-package com.jordan.model;
+// package com.jordan.model;
 
 
-import java.util.Set;
+// import java.util.Set;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// import javax.persistence.OneToMany;
+// import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.EqualsAndHashCode;
+// import lombok.NoArgsConstructor;
+// import lombok.ToString;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "productCategory")
-public class ProductCategory {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int id;
-	private String categoryName;
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// @Entity
+// @Table(name = "productCategory")
+// public class ProductCategory {
+// 	@Id
+// 	@GeneratedValue(strategy = GenerationType.AUTO)
+// 	private int id;
+// 	private String categoryName;
 
 
-}
+// }
