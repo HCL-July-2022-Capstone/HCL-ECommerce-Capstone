ecb0f085b5230a74528faaa72f8dfcadcb4af11a
David Gomez <davidgom00@gmail.com>
2022-09-21 14:40:17 -0500

    cloudinaryBackend

--- BackEnd/src/main/java/com/jordan/EcommerceApplication.java
+++ BackEnd/src/main/java/com/jordan/EcommerceApplication.java
@@ -1,8 +1,15 @@
 package com.jordan;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+
+import com.cloudinary.Cloudinary;
 
 @ServletComponentScan
 @SpringBootApplication
@@ -10,6 +17,27 @@ public class EcommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
+	}
+	
+	@Value("${cloudinary.cloud_name}")
+	private String cloudName;
+
+	@Value("${cloudinary.api_key}")
+	private String apiKey;
+
+	@Value("${cloudinary.api_secret}")
+	private String apiSecret;
+
+
+	@Bean
+	public Cloudinary cloudinaryConfig() {
+		Cloudinary cloudinary = null;
+		Map config = new HashMap();
+		config.put("cloud_name", cloudName);
+		config.put("api_key", apiKey);
+		config.put("api_secret", apiSecret);
+		cloudinary = new Cloudinary(config);
+		return cloudinary;
 	}
 
 }
