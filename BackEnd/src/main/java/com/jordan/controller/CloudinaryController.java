1f9eab04517c70144c735929ff34b0aec8971403
Sumaiya Shefa <sum.shefa@gmail.com>
2022-09-22 13:42:19 -0500

    Styling(8)

---
+++ BackEnd/src/main/java/com/jordan/controller/CloudinaryController.java
@@ -0,0 +1,22 @@
+package com.jordan.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.cloudinary.Cloudinary;
+import com.jordan.service.CloudinaryService;
+
+@RestController
+public class CloudinaryController {
+	private final CloudinaryService CloudinaryService = new CloudinaryService();
+
+//	@PostMapping("/upload")
+//	public String uploadFile(@RequestParam("file") MultipartFile f) {
+//        String url = CloudinaryService.uploadFile(f);
+//        return "File uploaded successfully: File path :  " + url;
+//    }
+
+}
